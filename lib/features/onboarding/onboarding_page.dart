6e99c91719835213f7ce16c8cfdce8e5eee33437
Davi Marques <davimarquesrosa@hotmail.com>
2025-02-16 18:37:16 -0300

    onboarding

---
+++ lib/features/onboarding/onboarding_page.dart
@@ -0,0 +1,88 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:financy_app/common/constants/app_colors.dart';
+import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.iceWhite,
+      body: Column(
+        children: [
+          const SizedBox(height: 64.0),
+          Expanded(
+            flex: 2,
+            child: Image.asset('assets/images/man.png'),
+          ),
+          _buildTitleText('Spend Smarter'),
+          _buildTitleText('Save More'),
+          const SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: PrimaryButton(
+              text: 'Get Started',
+              onPressed: () {},
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          Text(
+            'Already have an account? Log In',
+            style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+          ),
+          const SizedBox(height: 64.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTitleText(String text) {
+    return Text(
+      text,
+      style: AppTextStyles.mediumText.copyWith(color: AppColors.greelightTwo),
+    );
+  }
+}
+
+class PrimaryButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final String text;
+
+  const PrimaryButton({
+    super.key,
+    this.onPressed,
+    required this.text,
+  });
+
+  static const BorderRadius _borderRadius =
+      BorderRadius.all(Radius.circular(24.0));
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: _borderRadius,
+      onTap: onPressed,
+      child: Ink(
+        decoration: const BoxDecoration(
+          borderRadius: _borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.greenGradient,
+          ),
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          height: 48.0,
+          child: Text(
+            text,
+            style: AppTextStyles.mediumText18.copyWith(color: AppColors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
