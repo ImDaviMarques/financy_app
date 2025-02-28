6d3b74c7f8661cf1b8a92a1f7a6b0e36a3c731d4
Davi Marques <davimarquesrosa@hotmail.com>
2025-02-28 14:15:44 -0300

    Sign_Up_Page

---
+++ lib/features/sign_up/sign_up_page.dart
@@ -0,0 +1,63 @@
+import 'dart:developer';
+
+import 'package:financy_app/common/constants/app_colors.dart';
+import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/widgets/multi_text_button.dart';
+import 'package:financy_app/features/onboarding/onboarding_page.dart';
+
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      children: [
+        Text(
+          'Spend Smarter',
+          textAlign: TextAlign.center,
+          style: AppTextStyles.mediumText.copyWith(
+            color: AppColors.greelightTwo,
+          ),
+        ),
+        Text(
+          'Save More',
+          textAlign: TextAlign.center,
+          style: AppTextStyles.mediumText.copyWith(
+            color: AppColors.greelightTwo,
+          ),
+        ),
+        Image.asset(
+          'assets/images/sign_up_image.png',
+        ),
+        TextFormField(),
+        const TextField(),
+        Padding(
+          padding: const EdgeInsets.only(
+              left: 32.0, right: 32.0, top: 16.0, bottom: 4.0),
+          child: PrimaryButton(
+            text: 'Sign Up',
+            onPressed: () => log('Sign Up'),
+          ),
+        ),
+        const SizedBox(height: 16.0),
+        MultiTextButton(
+          onPressed: () => log('Sign Up'),
+          children: [
+            Text(
+              'Already have an account? ',
+              style: AppTextStyles.smallText.copyWith(color: AppColors.grey),
+            ),
+            Text(
+              'Log In',
+              style: AppTextStyles.smallText
+                  .copyWith(color: AppColors.greelightTwo),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
