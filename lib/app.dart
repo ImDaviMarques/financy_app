1eaaac101647282265582ef099559bc5003a2bcb
Davi Marques <davimarquesrosa@hotmail.com>
2025-03-01 14:46:03 -0300

    Start Sign Up

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:financy_app/common/themes/defaut_theme.dart';
+// import 'package:financy_app/common/themes/defaut_theme.dart';
 import 'package:financy_app/features/sign_up/sign_up_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: defaultTheme,
+      // theme: defaultTheme,
       home: SignUpPage(),
     );
   }
